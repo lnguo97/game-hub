09763c2bee44d98bff65782e77949f83f924873a
lnguo97 <lnguo97@outlook.com>
2026-02-09 16:08:48 +0800

    Fix destructuring of useGames and useGenres hooks to correctly reference data

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GamesGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data: games, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
