6cc01daae3a7f0eb90b2b4f97b09c1c3566a1d27
lnguo97 <lnguo97@outlook.com>
2026-02-11 09:07:52 +0800

    Add genres data and update useGenres hook to return static genre data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -7,6 +7,8 @@ export interface Genre {
   slug: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => {
+  return { data: genres, error: null, isLoading: false };
+};
 
 export default useGenres;
